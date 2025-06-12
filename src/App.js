f8ca21b0ad94fb332ba30e9612f1572f7c1d69cf
chino4242 <ryanj.contino@gmail.com>
2025-06-12 14:10:23 -0400

    Committed CustomerAddUpdateForm refactor

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect } from 'react';
 import {getAll, post, put, deleteById } from '../src/ProjectAssets/memdb';
 import CustomerList from '../src/components/CustomerList'
-import CustomerAddUpdateForm from '../src/components/CustomerList';
+import CustomerAddUpdateForm from '../src/components/CustomerAddUpdateForm';
 import "./App.css";
 
 function log(message){console.log(message);}
@@ -70,53 +70,17 @@ export function App(params) {
     selectedCustomerId = {formObject.id}
     handleListClick={handleListClick}
     />
-    <div className="boxed">
-      <div>
-        <h4>{mode}</h4>
-      </div>
-      <form >
-        <table id="customer-add-update" >
-          <tbody>
-            <tr>
-              <td className={'label'} >Name:</td>
-              <td><input
-                type="text"
-                name="name"
-                onChange={(e) => handleInputChange(e)}
-                value={formObject.name}
-                placeholder="Customer Name"
-                required /></td>
-            </tr>
-            <tr>
-              <td className={'label'} >Email:</td>
-              <td><input
-                type="email"
-                name="email"
-                onChange={(e) => handleInputChange(e)}
-                value={formObject.email}
-                placeholder="name@company.com" /></td>
-            </tr>
-            <tr>
-              <td className={'label'} >Pass:</td>
-              <td><input
-                type="text"
-                name="password"
-                onChange={(e) => handleInputChange(e)}
-                value={formObject.password}
-                placeholder="password" /></td>
-            </tr>
-            <tr className="button-bar">
-              <td colSpan="2">
-                <input type="button" value="Delete" onClick={onDeleteClick} />
-                <input type="button" value="Save" onClick={onSaveClick} />
-                <input type="button" value="Cancel" onClick={onCancelClick} />
-              </td>
-            </tr>
-          </tbody>
-        </table>
-      </form>
-    </div>
-    </div>
+    
+    <CustomerAddUpdateForm
+      formObject = {formObject}
+      mode = {mode}
+      handleInputChange={handleInputChange}
+      onDeleteClick = {onDeleteClick}
+      onSaveClick = {onSaveClick}
+      onCancelClick = {onCancelClick
+      }
+/>
+</div>
   );
 }
 
